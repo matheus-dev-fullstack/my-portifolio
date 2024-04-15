71044ed0b7b1d2a01cbc38cf693b951442dba1ba
Matheus Oliveira <118443175+matheus-dev-fullstack@users.noreply.github.com>
2024-04-15 15:32:50 +0000

    Aplicando ajustes de responsabilidade Mobile

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
     const StyledHero = styled("div")(() => ({
         backgroundColor: "Black",
-        height: "100vh"
+        height: "100vh",
     }))
     const StyledImg = styled("img")(() => ({
         width: "100%",
@@ -21,20 +21,21 @@ const Hero = () => {
             <StyledHero>
                 <Container>
                     <Grid container spacing={2}>
-                        <Grid item xs={4}>
+                        <Grid item xs={12} md={4}>
                             <StyledImg src={Avatar} />
                         </Grid>
-                        <Grid item xs={8}>
+                        <Grid item xs={12} md={8}>
                             <Typography color="primary" variant="h1" textAlign="center">Matheus Oliveira</Typography>
                             <Typography color="primary" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
-                            <Button>
-                                <DownloadIcon />
-                                DOWNLOAD CV
-                            </Button>
-                            <Button>
-                                <MailOutLineIcon />
-                                CONTACT ME
-                            </Button>
+                            <Grid container display="flex" justifyContent="center">
+                                <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                <Button>
+                                    <DownloadIcon />DOWNLOAD CV</Button>
+                                </Grid>
+                                <Grid item xs={12} md={4} display="flex" justifyContent="center"><Button><MailOutLineIcon />CONTACT ME</Button></Grid>
+                            </Grid>
+                            
+                            
                         </Grid>
                     </Grid>
                 </Container>
